10edbb3db789ab2438705db61b724c15511d19cd
xujunzhao <xujunzhao@xiaoniangao.com>
2021-04-29 22:30:48 +0800

    finish

--- week01/day01.go
+++ week01/day01.go
@@ -1,8 +1,28 @@
 package main
 
+import "fmt"
+
 /*
 	https://leetcode-cn.com/problems/two-sum/
 */
 func twoSum(nums []int, target int) []int {
+	if len(nums) <= 0 {
+		return nil
+	}
+
+	numMap := make(map[int]int, len(nums)) // key: 元素， value: index
+
+	for index, item:= range nums{
+		if _, ok := numMap[target-item]; ok {
+			return []int{index, numMap[target-item]}
+		} else {
+			numMap[item] = item
+		}
+	}
 	return nil
+}
+
+func main() {
+	res := twoSum([]int{2,7,11,15}, 9)
+	fmt.Println(res)
 }
