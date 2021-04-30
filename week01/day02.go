3754542322b46a2d54fbe589b85e010d6c236548
Zereker <zereker@Zerekers-Mac-mini.local>
2021-04-30 22:13:29 +0800

    ADD: Day2

--- week01/day02.go
+++ week01/day02.go
@@ -10,5 +10,37 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	return nil
+	var (
+		dummy = new(ListNode)
+		curr  = dummy
+
+		carry int
+	)
+
+	for l1 != nil || l2 != nil || carry > 0 {
+		curr.Next = new(ListNode)
+
+		curr = curr.Next
+		if l1 != nil {
+			carry += l1.Val
+			l1 = l1.Next
+		}
+
+		if l2 != nil {
+			carry += l2.Val
+			l2 = l2.Next
+		}
+
+		curr.Val = carry % 10
+		carry /= 10
+	}
+
+	return dummy.Next
+}
+
+func main() {
+	addTwoNumbers(
+		&ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3}}},
+		&ListNode{Val: 5, Next: &ListNode{Val: 6, Next: &ListNode{Val: 4}}},
+	)
 }
