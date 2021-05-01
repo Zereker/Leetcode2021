f33c128b384174df96b628e3faf490de23d2a9f8
whoops <walterlionking@163.com>
2021-05-02 00:15:44 +0800

    暴力破解

--- week01/day03.go
+++ week01/day03.go
@@ -1,5 +1,34 @@
 package main
 
+/**
+ * @Description:双重for循环暴力破解
+ */
 func lengthOfLongestSubstring(s string) int {
-	return 0
+	if len(s) == 1 {
+		return 1
+	}
+	temp := []byte(s)
+	result, begin := 0, 0
+	for begin < len(temp) {
+		tempMap := make(map[byte]bool)
+		tempLength := 0
+		for i := begin; i < len(temp); i++ {
+			if _, ok := tempMap[temp[i]]; !ok {
+				tempMap[temp[i]] = true
+				tempLength++
+			} else {
+				break
+			}
+			result = max(tempLength, result)
+		}
+		begin++
+	}
+
+	return result
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
